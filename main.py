b658457ef63c92c4d9fc09b814dc065704539e43
Daniyil Preen <daniyil.aurora@gmail.com>
2025-03-18 21:34:55 +0100

    Added player movement and player animation

--- main.py
+++ main.py
@@ -18,23 +18,46 @@ screen = pg.display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
 pg.display.set_caption("Platformer")
 
 p = player.Player(20, 20)
-testRect = block.Block(100, 50, "grass_block")
+testRect = block.Block(100, 250, "grass_block")
 
 # Main loop
 running = True
 clock = pg.time.Clock()
+tick = 0
 while running:
-    # Clear screen
-    screen.fill((255, 255, 255))
-
-    screen.blit(p.player, (p.rect.x, p.rect.y))
-    screen.blit(testRect.block, (testRect.rect.x, testRect.rect.y))
-
     #Events
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             running = False
     
+    # Tick calculation
+    if tick >= 60:
+        tick = 0
+
+    # Clear screen
+    screen.fill((255, 255, 255))
+
+    # Drawing player
+    screen.blit(p.player, (p.rect.x, p.rect.y))
+
+    #Player animation
+    p.updateAnimations(tick)
+
+    # Drawing test block
+    screen.blit(testRect.block, (testRect.rect.x, testRect.rect.y))
+
+    # Keyboard input
+    keys = pg.key.get_pressed()
+    if keys[pg.K_w] or keys[pg.K_UP]:
+        p.rect.y -= settings.PLAYER_SPEED
+    if keys[pg.K_a] or keys[pg.K_LEFT]:
+        p.rect.x -= settings.PLAYER_SPEED
+    if keys[pg.K_s] or keys[pg.K_DOWN]:
+        p.rect.y += settings.PLAYER_SPEED
+    if keys[pg.K_d] or keys[pg.K_RIGHT]:
+        p.rect.x += settings.PLAYER_SPEED
+    
     pg.display.flip()
     clock.tick(60)
+    tick += 1
--- player.py
+++ player.py
@@ -1,11 +1,19 @@
 import pygame as pg
 import settings as st
 
+animationsPaths = [
+    pg.image.load("imgs/player_idle_0.png"),
+    pg.image.load("imgs/player_idle_1.png")]
+
 class Player():
     def __init__(self, x, y):
-        self.player = pg.image.load("imgs/player.png").convert_alpha()
+        self.player = animationsPaths[0].convert_alpha()
         self.player = pg.transform.scale(self.player, (st.BLOCKS_WIDTH * 1.3, st.BLOCKS_HEIGHT * 1.3))
 
         self.rect = self.player.get_rect()
         self.rect.x = x
         self.rect.y = y
+
+    def updateAnimations(self, iteration):
+        self.player = animationsPaths[iteration // 30].convert_alpha()
+        self.player = pg.transform.scale(self.player, (st.BLOCKS_WIDTH * 1.3, st.BLOCKS_HEIGHT * 1.3))
