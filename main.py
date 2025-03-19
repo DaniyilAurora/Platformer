c05f56517935bf32fe25eba8077f81a407ca87bf
Daniyil Preen <daniyil.aurora@gmail.com>
2025-03-19 22:23:47 +0100

    Added walking animation

--- main.py
+++ main.py
@@ -35,14 +35,15 @@ while running:
     if tick >= 60:
         tick = 0
 
+    # Update player walking
+    p.walking = False
+
     # Clear screen
     screen.fill((255, 255, 255))
 
     # Drawing player
     screen.blit(p.player, (p.rect.x, p.rect.y))
 
-    #Player animation
-    p.updateAnimations(tick)
 
     # Drawing test block
     screen.blit(testRect.block, (testRect.rect.x, testRect.rect.y))
@@ -51,12 +52,21 @@ while running:
     keys = pg.key.get_pressed()
     if keys[pg.K_w] or keys[pg.K_UP]:
         p.rect.y -= settings.PLAYER_SPEED
+        p.walking = True
     if keys[pg.K_a] or keys[pg.K_LEFT]:
         p.rect.x -= settings.PLAYER_SPEED
+        p.walking = True
+        p.direction = "left"
     if keys[pg.K_s] or keys[pg.K_DOWN]:
         p.rect.y += settings.PLAYER_SPEED
+        p.walking = True
     if keys[pg.K_d] or keys[pg.K_RIGHT]:
         p.rect.x += settings.PLAYER_SPEED
+        p.walking = True
+        p.direction = "right"
+
+    #Player animation
+    p.updateAnimations(tick)
     
     pg.display.flip()
     clock.tick(60)
--- player.py
+++ player.py
@@ -1,19 +1,41 @@
 import pygame as pg
 import settings as st
 
-animationsPaths = [
+animationsIdle = [
     pg.image.load("imgs/player_idle_0.png"),
-    pg.image.load("imgs/player_idle_1.png")]
+    pg.image.load("imgs/player_idle_1.png")
+]
+
+animationsWalking = [
+    pg.image.load("imgs/player_walking_0.png"),
+    pg.image.load("imgs/player_walking_1.png"),
+    pg.image.load("imgs/player_walking_2.png"),
+    pg.image.load("imgs/player_walking_3.png"),
+    pg.image.load("imgs/player_walking_4.png"),
+    pg.image.load("imgs/player_walking_5.png"),
+]
 
 class Player():
     def __init__(self, x, y):
-        self.player = animationsPaths[0].convert_alpha()
+        self.player = animationsIdle[0].convert_alpha()
         self.player = pg.transform.scale(self.player, (st.BLOCKS_WIDTH * 1.3, st.BLOCKS_HEIGHT * 1.3))
 
         self.rect = self.player.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.walking = False
+        self.direction = "right"
 
     def updateAnimations(self, iteration):
-        self.player = animationsPaths[iteration // 30].convert_alpha()
-        self.player = pg.transform.scale(self.player, (st.BLOCKS_WIDTH * 1.3, st.BLOCKS_HEIGHT * 1.3))
+        if not self.walking:
+            self.player = animationsIdle[iteration // 30].convert_alpha()
+            self.player = pg.transform.scale(self.player, (st.BLOCKS_WIDTH * 1.3, st.BLOCKS_HEIGHT * 1.3))
+            
+            if self.direction == "left":
+                self.player = pg.transform.flip(self.player, True, False)
+        else:
+            self.player = animationsWalking[iteration // 5 % 6].convert_alpha()
+            self.player = pg.transform.scale(self.player, (st.BLOCKS_WIDTH * 1.3, st.BLOCKS_HEIGHT * 1.3))
+            
+            if self.direction == "left":
+                self.player = pg.transform.flip(self.player, True, False)
